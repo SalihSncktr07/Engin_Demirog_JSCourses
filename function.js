37e4faba28b9a22a6507cc848523012d84cf61a0
Salih <salihbaz07@gmail.com>
2021-08-20 17:28:21 +0300

    deneme

--- function.js
+++ function.js
@@ -1,6 +1,13 @@
-﻿var sayi1 = 6
-var sayi1 = 61
-var sayi1 = 31
-var sayi1 = 45
-var sayi1 = 17
-var sayi1 = 67
+﻿function sayiUret(){
+    
+}
+
+
+var sayi1 = Math.ceil(Math.random() * 15) // Math.random(random sayı üretir)     Math.ceil(sayı yuvarlar) 
+var sayi2 = 61
+var sayi3 = 31
+var sayi4 = 45
+var sayi5 = 17
+var sayi6 = 67
+
+console.log("Kolon :" + sayi1 +" "+ sayi2 +" "+ sayi3 +" "+ sayi4 +" "+ sayi5 +" "+ sayi6)
