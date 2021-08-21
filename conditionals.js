61283dca8332e401ae3998fe5fb9df1f01ff205c
Salih <salihbaz07@gmail.com>
2021-08-21 09:33:32 +0300

    conditionalsDemo.js file add

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+﻿var not = 79
+
+if (not>50){
+    console.log("Geçtiniz")
+}
+else if(not>=40){
+    console.log("Bütünlemeye kaldınız")
+}
+else {
+    console.log("Kaldınız")
+}
---
+++ conditionalsDemo.js
@@ -0,0 +1,13 @@
+﻿var sayi1 = 16
+var sayi2 = 45
+var sayi3 = 24
+
+// En büyük sayıyı bul
+var enBuyuk = sayi1 // 16
+if (enBuyuk<sayi2){
+    enBuyuk=sayi2   //45
+}
+if (enBuyuk<sayi3){
+    enBuyuk=sayi3   //24
+}
+console.log("Sayılardan en büyüğü: "+ enBuyuk)
