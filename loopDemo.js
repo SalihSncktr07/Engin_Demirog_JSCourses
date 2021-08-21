cc189678df8fdaaad1d86016d67794976d7ad69a
Salih <salihbaz07@gmail.com>
2021-08-21 11:27:39 +0300

    demo

---
+++ loopDemo.js
@@ -0,0 +1,10 @@
+﻿function sayiUret (ustLimit=50){
+    return  Math.ceil(Math.random()* ustLimit);
+}
+
+for (var i=1; i<=8; i++){
+    console.log(i +". KOLON");
+    for (var j=1; j<=6; j++){
+        console.log(sayiUret());
+    }
+}
