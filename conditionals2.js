7d21927498ad4841de0b13739e4c2869869d2922
Salih <salihbaz07@gmail.com>
2021-08-21 10:00:39 +0300

    conditionals2.js file add

---
+++ conditionals2.js
@@ -0,0 +1,11 @@
+﻿var sayi1 = 10
+var sayi2 = 10
+var sayi3 = 10
+
+if (sayi1 == sayi2 && sayi1 == sayi3){  //&& 've' her ikisininde koşulu sağlaması lazım
+    console.log("Eşit")
+}
+
+if (sayi1 == sayi2 || sayi1 == sayi3){  //|| 'veya' iki koşuldan birinin sağlaması yeter
+    console.log("Eşit")
+}
